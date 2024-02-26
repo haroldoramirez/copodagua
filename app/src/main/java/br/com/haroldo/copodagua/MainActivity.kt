bdce6fed37ad07d01bcd2ec2d64e7774ab5da7bc
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2024-02-25 22:38:23 -0300

    criado tela principal

---
+++ app/src/main/java/br/com/haroldo/copodagua/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.haroldo.copodagua
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
